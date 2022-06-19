1104fb2d4d0f12982e085f2a5efd1a8c9b3338e1
vitaliyivn27 <105597233+vitaliyivn27@users.noreply.github.com>
2022-06-19 19:25:04 +0300

    Add project files.

---
+++ Oop_1/Fractions.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Oop_1
+{
+    public class Fractions
+    {
+        public long Integer { get; set; }
+        public ushort FractionPart { get; set; }
+        int hmz = 0;
+
+
+        public Fractions(double num)
+        {
+            SetValues(num);
+        }
+
+        private void SetValues(double num)
+        {
+            Integer = (long)Math.Truncate(num);
+            FractionPart = GetFractionPart(num);
+
+        }
+
+        ushort GetFractionPart(double num)
+        {
+            ushort res;
+            try
+            {
+                var dStr = num.ToString().Split('.')[1];
+                foreach (var n in dStr.ToCharArray())
+                {
+                    if (n == '0')
+                    {
+                        hmz++;
+                    }
+                    else { break; }
+                }
+                res = Convert.ToUInt16(dStr);
+            }
+            catch { res = 0; }
+            return res;
+        }
+
+        string FractionPartString
+        {
+            get
+            {
+                var res = "";
+                for (int i = 0; i < hmz; i++)
+                {
+                    res += "0";
+                }
+                res += FractionPart;
+                return res;
+            }
+        }
+        public string ToString()
+        {
+            return string.Format("Integer: {0}; FractionPart: {1}. ", Integer, FractionPart);
+        }
+
+        public string Operations()
+        {
+            var FractionPartInt = Convert.ToInt64(FractionPartString);
+            var summury = Integer + FractionPartInt;
+            var substraction = Integer - FractionPartInt;
+            var multiplication = Integer * FractionPartInt;
+            
+            if (Integer > FractionPartInt)
+            {
+                return string.Format("Summury: {0}; Substraction: {1}; Multiplication: {2}; Integer > FractionPart.", summury, substraction, multiplication); 
+            }
+            else if (Integer < FractionPartInt)
+            {
+                return string.Format("Summury: {0}; Substraction: {1}; Multiplication: {2}; Integer < FractionPart.", summury, substraction, multiplication);
+            }
+            else
+            {
+                return string.Format("Summury: {0}; Substraction: {1}; Multiplication: {2}; Integer = FractionPart.", summury, substraction, multiplication);
+            }
+            
+        }
+    }
+}
---
+++ Oop_1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Oop_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter double number :");
+            var isDouble = double.TryParse(Console.ReadLine(), out double num);
+            if (isDouble)
+            {
+                Fractions firstNumber = new Fractions(num);
+                Console.WriteLine($"Your number : {num}");
+                Console.WriteLine(new Fractions(num).ToString());
+                Console.WriteLine(new Fractions(num).Operations());
+            }
+            else
+            {
+                Console.WriteLine("Wrong value");
+            }
+            
+        }
+    }
+}
